14c9a5af3b29d578ea8f319077be14b493eb4c9a
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-08-06 15:23:24 -0300

    add target serverless in next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "globals.scss";`
-  }
+    target: 'serverless',
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import "globals.scss";`,
+    },
 }

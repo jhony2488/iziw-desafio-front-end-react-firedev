6ef6e382049bde5afd1e30172a2fa33301a39aa0
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-08-09 13:05:02 -0300

    reconfig service component

--- components/molecules/Service.jsx
+++ components/molecules/Service.jsx
@@ -7,7 +7,7 @@ import styles from '../../styles/molecules/Service.module.scss'
 
 export default function Service(props) {
     return (
-        <div id={`service-${  props.keyService}`} className={styles.service}>
+        <div id={`service-${props.keyService}`} className={styles.service}>
             <Image src={serviceImage} alt={props.title} className={styles.service__image} />
             <h2>{props.title}</h2>
         </div>

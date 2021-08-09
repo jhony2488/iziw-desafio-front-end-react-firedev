a8e7893ac26e07d346d431e8f8cab19b8f582ea7
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-08-09 02:02:26 -0300

    service component created

---
+++ components/molecules/Service.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+import serviceImage from '../../public/images/service.png'
+import styles from '../../styles/molecules/Service.module.scss'
+
+
+export default function Service(props) {
+    return (
+        <div id={`service-${  props.keyService}`} className={styles.service}>
+            <Image src={serviceImage} alt={props.title} className={styles.service__image} />
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
